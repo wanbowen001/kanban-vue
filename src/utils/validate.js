25677724130e180dc670f9d6c1017bd278675b63
wanbowen <wanbw@m-chain.com>
2020-07-21 20:12:47 +0800

    new add - 民航看板系统上传

---
+++ src/utils/validate.js
@@ -0,0 +1,39 @@
+import { userNameList } from '@/api/login'
+
+export function isvalidUsername(str) {
+  // 从后台返回账户数据
+  // const valid_map = ['admin', 'test', 'wanbowen']
+  // const valid_map = userNameList().then( res => {
+  //   const data = res.data
+  //   return data
+  // }).catch( error => {
+  //   alert(error)
+  // })
+  // return valid_map.indexOf(str.trim()) >= 0
+  return true
+}
+
+/* 合法uri*/
+export function validateURL(textval) {
+  const urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/
+  return urlregex.test(textval)
+}
+
+/* 小写字母*/
+export function validateLowerCase(str) {
+  const reg = /^[a-z]+$/
+  return reg.test(str)
+}
+
+/* 大写字母*/
+export function validateUpperCase(str) {
+  const reg = /^[A-Z]+$/
+  return reg.test(str)
+}
+
+/* 大小写字母*/
+export function validatAlphabets(str) {
+  const reg = /^[A-Za-z]+$/
+  return reg.test(str)
+}
+
